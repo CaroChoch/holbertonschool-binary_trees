f11dbae2ed63d5ff1e3a6c42ce17b3944c259e32
soOwasTaken <444deph12@gmail.com>
2023-01-12 14:05:13 +0000

    trying task 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - function search node sibling
+ * of a binary tree.
+ * @node: pointer to the node
+ *
+ * Return: 0 if tree is NULL.
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if ((node == NULL || node->parent == NULL || node->parent->parent == NULL))
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->parent->left);
+	return (node->parent->parent->right);
+}
